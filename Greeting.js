0c0bc7fc590bc7b4a832cb0154aafbc46f9f88cf
Elvis Iradukunda <student@Students-MacBook-Pro.local>
2024-08-28 14:33:42 +1200

    JS103 exercise

---
+++ Greeting.js
@@ -0,0 +1,8 @@
+console.log("Greeting Generator")
+
+function greeting(Roshi){
+    console.log(`Yo Whatsup, Mr ${Roshi}`);
+    console.log("whats crackin");
+    console.log("Homeboyy");
+}
+greeting("Roshi");
---
+++ Math wizard.js
@@ -0,0 +1,22 @@
+console.log ("Math wizard");
+function add(num1, num2) {
+    return num1 + num2;
+}
+console.log(add(2, 3));
+function sub(num1, num2) {
+    return num1 - num2;
+}
+console.log(sub(5, 3));
+function mult(num1, num2) {
+    return num1 * num2;
+}
+console.log(mult(3, 3));
+function div(num1, num2) {
+    return num1 / num2;
+}
+console.log(div(8, 2));
+
+function calculate(a, b) {
+    return 50 + 50 ;
+  }
+  console.log( 50+50);
